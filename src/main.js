e98b9c98867a42d0df38085d8e262e24d6428fec
dahby <dstoll243@gmail.com>
2018-05-22 13:03:07 -0700

    built out webpack js files per lecture code

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { render as reactDomRender } from 'react-dom';
+import superagent from 'superagent';
+import './style/main.scss';
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const merge = require('webpack-merge');
+const commonConfig = require('./webpack.common');
+const { HotModuleReplacementPlugin } = require('webpack');
+
+const webpackDevConfig = {};
+
+webpackDevConfig.mode = 'development';
+webpackDevConfig.devtool = 'inline-source-map';
+
+webpackDevConfig.devServer = {
+  contentBase: './build',
+  open: true,
+  hot: true,
+  historyApiFallback: true,
+};
+
+webpackDevConfig.plugins = [
+  {
+    test: /\.scss$/,
+    use: [
+      'style-loader',
+      'css-loader',
+      'sass-loader',
+    ],
+  },
+];
+
+module.exports = merge(commonConfig, webpackDevConfig);
